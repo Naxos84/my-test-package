3e1cb96fd3d623ce9630685370d6961dbb6cdb42
Matthias Kolley <matthias.kolley@googlemail.com>
2022-08-22 19:14:03 +0000

    change structure

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const myFunctions = {
-  sum(a, b) {
-    console.log("my sum");
-    return a + b;
-  },
-
-  subtract(a, b) {
-    console.log("my subtraction");
-    return a - b;
-  },
-
-  multiply(a, b) {
-    console.log("my product");
-    return a * b;
-  },
-  divide(a, b) {
-    console.log("my division");
-    return a / b;
-  },
-};
-
-export default myFunctions;
+define("index", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    const myFunctions = {
+        sum(a, b) {
+            console.log("my sum");
+            return a + b;
+        },
+        subtract(a, b) {
+            console.log("my subtraction");
+            return a - b;
+        },
+        multiply(a, b) {
+            console.log("my product");
+            return a * b;
+        },
+        divide(a, b) {
+            console.log("my division");
+            return a / b;
+        },
+    };
+    exports.default = myFunctions;
+});
---
+++ index.ts
@@ -0,0 +1,22 @@
+const myFunctions = {
+  sum(a: number, b: number) {
+    console.log("my sum");
+    return a + b;
+  },
+
+  subtract(a: number, b: number) {
+    console.log("my subtraction");
+    return a - b;
+  },
+
+  multiply(a: number, b: number) {
+    console.log("my product");
+    return a * b;
+  },
+  divide(a: number, b: number) {
+    console.log("my division");
+    return a / b;
+  },
+};
+
+export default myFunctions;
