6b39910aff162bfd407a763da3f41e9d5b12e20b
Matthias Kolley <matthias.kolley@googlemail.com>
2022-08-22 18:40:42 +0000

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+const myFunctions = {
+  sum(a, b) {
+    console.log("my sum");
+    return a + b;
+  },
+
+  subtract(a, b) {
+    console.log("my subtraction");
+    return a - b;
+  },
+
+  multiply(a, b) {
+    console.log("my product");
+    return a * b;
+  },
+  divide(a, b) {
+    console.log("my division");
+    return a / b;
+  },
+};
+
+export default myFunctions;
